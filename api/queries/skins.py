51382066b8455d9dd4c0e1a56882407537cd56c2
Zachary <loftis03@gmail.com>
2024-02-09 13:18:15 -0800

    Created a route get specific skins by id

--- api/queries/skins.py
+++ api/queries/skins.py
@@ -18,3 +18,14 @@ class SkinQueries:
             print(f"Error retrieving skins list: {e}")
 
         # return None
+    def get_skin_id(self, skin_id: str):
+        print(skin_id, "THIS IS SKIN ID")
+        url = (
+                "https://bymykel.github.io/CSGO-API/api/en/skins.json"
+            )
+        response = requests.get(url)
+        skin_list = response.json()
+        for dict in skin_list:
+            if dict['id'] == skin_id:
+                print(dict, "THIS IS DICT")
+                return dict
--- api/routers/skins.py
+++ api/routers/skins.py
@@ -8,3 +8,10 @@ router = APIRouter()
 @router.get("/api/skins")
 async def get_all_skins(queries: SkinQueries = Depends()):
     return queries.get_all_skins()
+
+
+@router.get("/api/skins/{skin_id}", response_model=dict)
+def get_skin_id(skin_id: str, queries: SkinQueries = Depends()):
+    potatoe = queries.get_skin_id(skin_id)
+    print(potatoe, "THIS IS POTATOE")
+    return potatoe
